80aee1f2704b3019959e6638bca0c19ea7ebef9f
Jason Faria <faria.jason@gmail.com>
2024-12-04 17:38:12 -0800

    Added comments to homebrew uninstall

--- tests/uninstall-homebrew.sh
+++ tests/uninstall-homebrew.sh
@@ -6,6 +6,7 @@ curl -sLO https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh
 chmod +x ./uninstall.sh
 sudo ./uninstall.sh --force
 
+# Remove Homebrew directories
 sudo rm -rf /usr/local/Homebrew
 sudo rm -rf /usr/local/Caskroom
 sudo rm -rf /usr/local/bin/brew
