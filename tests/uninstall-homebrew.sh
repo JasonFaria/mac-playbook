8b04f6760cb4d47c1d540a76e2ff95a509cbb461
Jason Faria <faria.jason@gmail.com>
2024-12-04 17:32:19 -0800

    Add tests

---
+++ tests/uninstall-homebrew.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Use official Homebrew uninstall script
+curl -sLO https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh
+
+chmod +x ./uninstall.sh
+sudo ./uninstall.sh --force
+
+sudo rm -rf /usr/local/Homebrew
+sudo rm -rf /usr/local/Caskroom
+sudo rm -rf /usr/local/bin/brew
+sudo rm -rf /opt/homebrew
